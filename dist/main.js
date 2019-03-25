15743174af1d54916adfec240d1dfc84dd0b7ea5
Isaque Alves <isaque.mace@gmail.com>
2019-03-24 23:16:33 -0300

    Primeiro passo com o restigy: config server, http get e config router

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,16 @@
-console.log('main script...');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const server = restify.createServer({
+    name: 'meat-api',
+    version: '1.0.0'
+});
+server.get('/hello', (req, resp, next) => {
+    resp.json({
+        message: 'hello'
+    });
+    return next();
+});
+server.listen(3000, () => {
+    console.log('Api is running on http://localhost:3000');
+});
