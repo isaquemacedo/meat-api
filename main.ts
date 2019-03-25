c02104182b42aeedc8a64b30e1f0b1789130da21
Isaque Alves <isaque.mace@gmail.com>
2019-03-24 23:35:33 -0300

    Compreendendo os três metodos do restify (response, request e next), as três opções do next e como o restify retornar as proprieades e metodos

--- main.ts
+++ main.ts
@@ -5,13 +5,35 @@ const server = restify.createServer({
     version: '1.0.0'
 })
 
-server.get('/hello', (req, resp, next) => {
+server.use(restify.plugins.queryParser())
+
+server.get('/info', [(req, resp, next) => {
+    if(req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
+        //resp.status(400)
+        //resp.send({message: 'Please, update your browser'})
+        let error: any = new Error()
+        error.statusCode = 400
+        error.message = 'Please, update your browser'
+        return next(error)
+    }
+    return next()
+},
+(req, resp, next) => {
+    // resp.contentType = 'application/json'
+    // resp.setHeader('Content-Type', 'application/json');
+    // resp.status(201)
+    // resp.send({message: 'hello'})
+    
     resp.json({
-        message: 'hello'
-    })
+        browser: req.userAgent(),
+        method: req.method,
+        url: req.href(),
+        path: req.path(),
+        query: req.query
+    })    
 
     return next()
-})
+}]);
 
 server.listen(3000, () =>{
     console.log('Api is running on http://localhost:3000')
