2aa81ac0d70b21aabb55711bd5cc3c7efc563fab
Isaque Alves <isaque.mace@gmail.com>
2019-03-25 00:39:28 -0300

    Criado get para recurso unico (findById)

--- common/router.ts
+++ common/router.ts
@@ -3,32 +3,3 @@ import * as restify from 'restify'
 export abstract class Router {
     abstract applyRoutes(application: restify.Server)
 }
-
-
-// this.application.get('/info', [(req, resp, next) => {
-//     if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
-//       //resp.status(400)
-//       //resp.send({message: 'Please, update your browser'})
-//       let error: any = new Error()
-//       error.statusCode = 400
-//       error.message = 'Please, update your browser'
-//       return next(error)
-//     }
-//     return next()
-//   },
-//   (req, resp, next) => {
-//     // resp.contentType = 'application/json'
-//     // resp.setHeader('Content-Type', 'application/json');
-//     // resp.status(201)
-//     // resp.send({message: 'hello'})
-
-//     resp.json({
-//       browser: req.userAgent(),
-//       method: req.method,
-//       url: req.href(),
-//       path: req.path(),
-//       query: req.query
-//     })
-
-//     return next()
-//   }]);
--- dist/common/router.js
+++ dist/common/router.js
@@ -3,28 +3,3 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Router {
 }
 exports.Router = Router;
-// this.application.get('/info', [(req, resp, next) => {
-//     if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
-//       //resp.status(400)
-//       //resp.send({message: 'Please, update your browser'})
-//       let error: any = new Error()
-//       error.statusCode = 400
-//       error.message = 'Please, update your browser'
-//       return next(error)
-//     }
-//     return next()
-//   },
-//   (req, resp, next) => {
-//     // resp.contentType = 'application/json'
-//     // resp.setHeader('Content-Type', 'application/json');
-//     // resp.status(201)
-//     // resp.send({message: 'hello'})
-//     resp.json({
-//       browser: req.userAgent(),
-//       method: req.method,
-//       url: req.href(),
-//       path: req.path(),
-//       query: req.query
-//     })
-//     return next()
-//   }]);
