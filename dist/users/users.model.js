6cc76e91a2a47cf22398008ebd36d393e56bd4c2
Isaque Alves <isaque.mace@gmail.com>
2019-05-04 13:27:19 -0300

    Adicionando validações para metódos de query e criada as middlewares para update and put

--- dist/users/users.model.js
+++ dist/users/users.model.js
@@ -36,16 +36,30 @@ const userSchema = new mongoose.Schema({
         }
     }
 });
-userSchema.pre('save', function (next) {
+const hashPassword = (obj, next) => {
+    bcrypt.hash(obj.password, environment_1.environment.security.saltRounds).then(hash => {
+        obj.password = hash;
+        next();
+    }).catch(next);
+};
+const saveMiddleware = function (next) {
     const user = this;
     if (!user.isModified('password')) {
         next();
     }
     else {
-        bcrypt.hash(user.password, environment_1.environment.security.saltRounds).then(hash => {
-            user.password = hash;
-            next();
-        }).catch(next);
+        hashPassword(user, next);
     }
-});
+};
+const updateMiddleware = function (next) {
+    if (!this.getUpdate().password) {
+        next();
+    }
+    else {
+        hashPassword(this.getUpdate(), next);
+    }
+};
+userSchema.pre('save', saveMiddleware);
+userSchema.pre('findOneAndUpdate', updateMiddleware);
+userSchema.pre('update', updateMiddleware);
 exports.User = mongoose.model('User', userSchema);
